b48b909669fcb87aec3cd2fead8b32c6dc18db6e
Madina-Aleroeva <126339295+Madina-Aleroeva@users.noreply.github.com>
2024-03-20 18:02:57 +0300

    Add files via upload

--- src/main/java/ru/practicum/shareit/error/ErrorHandler.java
+++ src/main/java/ru/practicum/shareit/error/ErrorHandler.java
@@ -2,13 +2,8 @@ package ru.practicum.shareit.error;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.ResponseStatus;
-import org.springframework.web.bind.annotation.RestControllerAdvice;
-import ru.practicum.shareit.exception.DuplicateException;
-import ru.practicum.shareit.exception.EmptyException;
-import ru.practicum.shareit.exception.NotFoundException;
-import ru.practicum.shareit.exception.ValidationException;
+import org.springframework.web.bind.annotation.*;
+import ru.practicum.shareit.exception.*;
 
 @RestControllerAdvice
 @Slf4j
@@ -28,12 +23,6 @@ public class ErrorHandler {
     @ExceptionHandler
     @ResponseStatus(HttpStatus.NOT_FOUND)
     public ErrorResponse validationException(final NotFoundException e) {
-        return new ErrorResponse(e.getMessage());
-    }
-
-    @ExceptionHandler
-    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
-    public ErrorResponse validationException(final EmptyException e) {
         return new ErrorResponse(e.getMessage());
     }
 
